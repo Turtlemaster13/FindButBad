0a482b7d98ae28ba403db1c304f68c585fc73ab9
turtlemaster13 <olivergiordano2@gmail.com>
2023-08-13 09:32:48 -0700

    made it work fixed seg fault

--- badFind.c
+++ badFind.c
@@ -5,12 +5,11 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 void walkDirectory(char *dir, int curDepth, int maxDepth, char * searchString)
 {	
-	printf("Searching for %s in %s\n", searchString, dir);
-
 	struct stat statPath;
 	if (curDepth > maxDepth) {
 		return;
@@ -18,43 +17,46 @@ void walkDirectory(char *dir, int curDepth, int maxDepth, char * searchString)
 	curDepth++;
 
 	// Open the local directory and look for the file
-
 	DIR *openDir = opendir(dir);
 	if (openDir == NULL) {
 		printf("error");
+		return;
 	}
 	struct dirent *entry;
 	while ((entry = readdir(openDir)) != NULL) {
-		printf("%s\n", entry->d_name);
-		if (strncmp(entry->d_name, searchString, strlen(searchString))== 0){
+		if ( strncmp(entry->d_name, "..", strlen("..")) == 0  || strncmp(entry->d_name, ".", strlen("."))== 0){
+			continue;
+		}
+		if (strncmp(entry->d_name, searchString, strlen(searchString)) == 0){
 			printf("found file : [%s/%s]\n", dir, searchString);
+			closedir(openDir);
 			exit(0);
 		}
 	}
 
 	// Find local directories and search those
 	rewinddir(openDir);
-	struct dirent *entry2;
-	while ((entry2 = readdir(openDir)) != NULL) {
-		printf("checking [%s] - %i\n", entry2->d_name, strncmp(entry2->d_name, "..", strlen("..")));
-		if ( strncmp(entry2->d_name, "..", strlen("..")) == 0  || strncmp(entry2->d_name, ".", strlen("."))== 0){
+	while ((entry = readdir(openDir)) != NULL) {
+		if ( strncmp(entry->d_name, "..", strlen("..")) == 0  || strncmp(entry->d_name, ".", strlen("."))== 0){
 			continue;
-		}else{
-			char cwd[500];
-			getcwd(cwd, sizeof(cwd));
-			stat(entry2->d_name, &statPath);
-
-			char checkPath[1024];
-			sprintf(checkPath, "%s/%s", dir, entry2->d_name);
-			stat(checkPath, &statPath);
-
-			if (S_ISDIR(statPath.st_mode)){
-				return walkDirectory(checkPath, curDepth, maxDepth, searchString);
-			} else {
-			}
+		}
+		char cwd[128];
+		getcwd(cwd, sizeof(cwd));
+		char checkPath[512];
+		if (strcmp(dir, "/") == 0){
+			sprintf(checkPath, "%s%s", dir, entry->d_name);
+		} else {
+			sprintf(checkPath, "%s/%s", dir, entry->d_name);
+		}
+		stat(checkPath, &statPath);
+		if (S_ISDIR(statPath.st_mode)){
+			printf("checking [%s, %s]\n", dir, entry->d_name);
+			walkDirectory(checkPath, curDepth, maxDepth, searchString);
+		
 		}
 	}
 	closedir(openDir);
+	return;
 }
 
 
@@ -62,8 +64,12 @@ void walkDirectory(char *dir, int curDepth, int maxDepth, char * searchString)
 int main(int argc, char ** argv)
 {	
 	char * startDirectory;
-	int maxDepth = 3;
+	int maxDepth = 6;
 	int found = 0;
-	walkDirectory(argv[2], 1, maxDepth, argv[1]);
+	if (strncmp(argv[1], "--help", strlen("--help")) == 0){
+		printf("----finds a file----\narg[1]: start directory\narg[2]: the file to find\n");
+	} else {
+		walkDirectory(argv[1], 1, maxDepth, argv[2]);
+	}
 	return 0;
 }
